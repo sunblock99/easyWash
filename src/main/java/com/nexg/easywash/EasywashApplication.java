f3996da71ddbb315d73251d9dcb2c6cff53ffa14
선형채 <sunblock99@kakao.com>
2024-02-07 15:08:08 +0900

    Initial commit

---
+++ src/main/java/com/nexg/easywash/EasywashApplication.java
@@ -0,0 +1,13 @@
+package com.nexg.easywash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasywashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasywashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexg/easywash/EasywashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexg.easywash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasywashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
